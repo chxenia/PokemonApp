f9f89ea9e441b15943bb16425ac3d512964d53ea
chxenia <chrnxenia@gmail.com>
2024-02-24 22:27:38 +0300

    added changes

--- TestPokemonApp/Modules/Entities/Pokemon.swift
+++ TestPokemonApp/Modules/Entities/Pokemon.swift
@@ -6,29 +6,12 @@
 //
 
 import Foundation
+import CoreData
 
 struct Pokemon: Codable {
     var name: String
     var url: String
 }
 
-struct PokemonInfo: Codable {
-    let name: String
-    let sprites: Sprites
-    let types: [TypeElement]
-    let weight: Double
-    let height: Double
-    
-    struct Sprites: Codable {
-        let front_default: String?
-    }
-    
-    struct TypeElement: Codable {
-        let type: TypeClass
-    }
 
-    struct TypeClass: Codable {
-        let name: String
-    }
-}
 
--- TestPokemonApp/Modules/Entities/PokemonInfo.swift
+++ TestPokemonApp/Modules/Entities/PokemonInfo.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import CoreData
+
+struct PokemonInfo: Codable {
+    let name: String
+    let sprites: Sprites
+    let types: [TypeElement]
+    let weight: Double
+    let height: Double
+    
+    struct Sprites: Codable {
+        let front_default: String?
+    }
+    
+    struct TypeElement: Codable {
+        let type: TypeClass
+    }
+
+    struct TypeClass: Codable {
+        let name: String
+    }
+}
